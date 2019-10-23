e73a57287572b51acd3178098af0ed4745aa4e6d
saltykevin <kevin.lee@saltedstone.com>
2019-10-22 18:18:11 -0700

    init

---
+++ Data Structures/stack.js
@@ -0,0 +1,58 @@
+// Stack class
+class Stack {
+
+    // Array is used to implement stack
+    constructor(){
+        this.items = [];
+    }
+
+    // push function
+    push(element) {
+        // push element into the items
+        this.items.push(element);
+    }
+
+    // pop function
+    pop() {
+        // return top most element in the stack
+        // and removes it from the stack
+        // Underflow if stack is empty
+        if (this.items.length == 0)
+            return "Underflow";
+        return this.items.pop();
+    }
+
+    // peek function
+    peek() {
+        // return the top most element from the stack
+        // but does'nt delete it.
+        return this.items[this.items.length - 1];
+    }
+
+    // isEmpty function
+    isEmpty() {
+        // return true if stack is empty
+        return this.items.length == 0;
+    }
+
+    // printStack function
+    printStack() {
+        var str = "";
+        for (var i = 0; i < this.items.length; i++)
+            str += this.items[i] + " ";
+        return str;
+    }
+}
+
+
+
+// creating object for stack class
+var stack = new Stack();
+
+// testing isEmpty and pop on an empty stack
+
+// returns false
+console.log(stack.isEmpty());
+
+// returns Underflow
+console.log(stack.pop());
