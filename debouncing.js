f21f36e2246c6d0bface9153238ced38e1d4fe3b
saltykevin <kevin.lee@saltedstone.com>
2019-10-25 13:28:13 -0700

    Update debouncing.js

--- debouncing.js
+++ debouncing.js
@@ -2,9 +2,13 @@ function debounce(f, t) {
   return function (args) {
     let previousCall = this.lastCall;
     this.lastCall = Date.now();
+
+    // if call happened again before the timer completed, start over
     if (previousCall && ((this.lastCall - previousCall) <= t)) {
       clearTimeout(this.lastCallTimer);
     }
+
+    // set the timeout to the argument "t"
     this.lastCallTimer = setTimeout(() => f(args), t);
   }
 }
