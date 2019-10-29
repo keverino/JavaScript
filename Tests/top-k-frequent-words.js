c4466be07afe61c1914b7c160e20c95218404278
saltykevin <kevin.lee@saltedstone.com>
2019-10-29 10:08:30 -0700

    init

---
+++ Tests/top-k-frequent-words.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    var words = words.sort();
+    var frequency = {};
+
+    for(var i = 0; i < words.length; i++){
+        // if there is nothing in this location, set key to word string and value to 1
+        if( !frequency[words[i]] )
+            frequency[words[i]] = 1;
+        // otherwise increment the counter for this key (the word)
+        else
+            frequency[words[i]]++;
+    }
+
+    console.log(frequency);
+
+    var arr = [];
+    for (var key in frequency) {
+      arr.push({key: key, value: frequency[key]});
+    }
+
+    // sort by the value, where the highest count is first
+    arr.sort(function(a, b) {
+        return b.value - a.value;
+    });
+
+
+    var ans = [];
+    for (var i = 0; i < k; i++)
+        ans.push(arr[i].key);
+
+    return ans;
+};
